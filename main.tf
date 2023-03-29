a99198fcaabd83f78c05f53c08ede1fc856ce970
Murthy <murthychiluka@gmail.com>
2023-03-29 22:26:52 +0530

    d

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 resource "aws_elasticache_cluster" "elasticache" {
-  cluster_id        = "${var.env}-elasticache"
-  engine            = var.engine
-  node_type         = var.node_type
-  num_cache_nodes   = var.num_cache_nodes
-  engine_version    = var.engine_version
-  port              = 6379
-  subnet_group_name = aws_elasticache_subnet_group.main.name
+  cluster_id         = "${var.env}-elasticache"
+  engine             = var.engine
+  node_type          = var.node_type
+  num_cache_nodes    = var.num_cache_nodes
+  engine_version     = var.engine_version
+  port               = 6379
+  subnet_group_name  = aws_elasticache_subnet_group.main.name
+  security_group_ids = [aws_security_group.main.id]
 
   tags = merge(
     var.tags,
